b8322953bb5dae3584603b2c4a61364750cb744c
litianqi <litianqi@yy.com>
2019-05-15 15:08:55 +0800

    update

---
+++ TQHtmlParseDemo/TQHtmlParseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TQHtmlParseDemo
+//
+//  Created by litianqi on 2019/5/15.
+//  Copyright © 2019 edu24ol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TQHtmlParseDemo/TQHtmlParseDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TQHtmlParseDemo
+//
+//  Created by litianqi on 2019/5/15.
+//  Copyright © 2019 edu24ol. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
